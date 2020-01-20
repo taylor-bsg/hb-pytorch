28552d0047a6ad233d654d362b988883c64f1dfe
lc873 <lc873@cornell.edu>
2020-01-19 23:22:53 -0500

    [CUDALiteTensor] add a few tensor creation functions

---
+++ aten/src/ATen/native/cudalite/TensorFactories.cpp
@@ -0,0 +1,74 @@
+// define constants like M_PI and C keywords for MSVC
+#ifdef _MSC_VER
+#define _USE_MATH_DEFINES
+#include <math.h>
+#endif
+
+#include <ATen/ATen.h>
+#include <ATen/CPUGenerator.h>
+#include <ATen/Utils.h>
+#include <ATen/Dispatch.h>
+#include <ATen/NativeFunctions.h>
+#include <c10/core/ScalarType.h>
+#include <c10/util/Deprecated.h>
+#include <ATen/native/Resize.h>
+#include <ATen/native/TensorFactories.h>
+#include <c10/core/TensorOptions.h>
+#include <TH/THAllocator.h>
+#include <ATen/detail/CUDAHooksInterface.h>
+#include <c10/util/Exception.h>
+#include <ATen/NamedTensorUtils.h>
+#include <ATen/core/EnableNamedTensor.h>
+
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <cstddef>
+#include <string>
+
+namespace at {
+namespace native {
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ empty ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Tensor empty_cudalite(IntArrayRef size, const TensorOptions& options, c10::optional<c10::MemoryFormat> optional_memory_format) {
+  AT_ASSERT(options.device().type() == DeviceType::CPU);
+  TORCH_INTERNAL_ASSERT(impl::variable_excluded_from_dispatch());
+  check_size_nonnegative(size);
+
+  c10::Allocator* allocator;
+  if (options.pinned_memory()) {
+    allocator = detail::getCUDAHooks().getPinnedMemoryAllocator();
+  } else {
+    allocator = at::getCPUAllocator();
+  }
+
+  int64_t nelements = prod_intlist(size);
+  auto dtype = options.dtype();
+  auto storage_impl = c10::make_intrusive<StorageImpl>(
+    dtype,
+    nelements,
+    allocator->allocate(nelements * dtype.itemsize()),
+    allocator,
+    /*resizeable=*/true);
+
+  auto tensor = detail::make_tensor<TensorImpl>(std::move(storage_impl), at::TensorTypeId::CUDALiteTensorId);
+  // Default TensorImpl has size [0]
+  if (size.size() != 1 || size[0] != 0) {
+    tensor.unsafeGetTensorImpl()->set_sizes_contiguous(size);
+  }
+
+  auto memory_format = optional_memory_format.value_or(MemoryFormat::Contiguous);
+  tensor.unsafeGetTensorImpl()->empty_tensor_restride(memory_format);
+  return tensor;
+}
+
+
+Tensor empty_strided_cudalite(IntArrayRef size, IntArrayRef stride, const TensorOptions& options) {
+  check_size_nonnegative(size);
+  auto t = at::native::empty_cudalite({0}, options);
+  at::native::resize_impl_cpu_(t.unsafeGetTensorImpl(), size, stride);
+  return t;
+}
+
+} // namespace native
+} // namespace at
