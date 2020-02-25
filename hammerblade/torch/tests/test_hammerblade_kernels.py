c94d4a6e48b84e922868b040c02863e1477156c2
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-02-24 22:43:58 -0800

    view tests

--- hammerblade/torch/tests/test_hammerblade_kernels.py
+++ hammerblade/torch/tests/test_hammerblade_kernels.py
@@ -156,7 +156,7 @@ def test_elementwise_div_1():
   z = x * y
   z_h = x.hammerblade() / y.hammerblade()
   assert z_h.device == torch.device("hammerblade")
-  assert torch.equal(z_h.cpu(), z)
+  assert torch.allclose(z_h.cpu(), z)
 
 def test_elementwise_div_2():
   x = torch.ones(4,5)
@@ -164,7 +164,7 @@ def test_elementwise_div_2():
   z = x / y
   z_h = x.hammerblade() / y.hammerblade()
   assert z_h.device == torch.device("hammerblade")
-  assert torch.equal(z_h.cpu(), z)
+  assert torch.allclose(z_h.cpu(), z)
 
 def test_elementwise_div_3():
   x = torch.rand(1,128)
@@ -172,7 +172,7 @@ def test_elementwise_div_3():
   z = x / y
   z_h = x.hammerblade() / y.hammerblade()
   assert z_h.device == torch.device("hammerblade")
-  assert torch.equal(z_h.cpu(), z)
+  assert torch.allclose(z_h.cpu(), z)
 
 def test_elementwise_div_4():
   x = torch.rand(16,32)
@@ -180,7 +180,7 @@ def test_elementwise_div_4():
   z = x / y
   z_h = x.hammerblade() / y.hammerblade()
   assert z_h.device == torch.device("hammerblade")
-  assert torch.equal(z_h.cpu(), z)
+  assert torch.allclose(z_h.cpu(), z)
 
 def test_elementwise_in_place_div():
   x1 = torch.rand(16,32)
@@ -191,5 +191,45 @@ def test_elementwise_in_place_div():
   x1_h.div_(x2_h)
   assert x1_h.device == torch.device("hammerblade")
   x1_h_c = x1_h.cpu()
-  assert torch.equal(x1_h_c, x1)
+  assert torch.allclose(x1_h_c, x1)
+
+def test_view_1():
+  x1 = torch.rand(2,3)
+  x1_h = x1.hammerblade()
+
+  x1 = x1.view(2*3)
+  x1_h = x1_h.view(2*3)
+
+  assert x1_h.device == torch.device("hammerblade")
+  assert torch.equal(x1_h.cpu(), x1)
+
+def test_view_2():
+  x1 = torch.rand(2,3)
+  x1_h = x1.hammerblade()
+
+  x1 = x1.view(1, 2*3)
+  x1_h = x1_h.view(1, 2*3)
+
+  assert x1_h.device == torch.device("hammerblade")
+  assert torch.equal(x1_h.cpu(), x1)
+
+def test_view_3():
+  x1 = torch.rand(4,6)
+  x1_h = x1.hammerblade()
+
+  x1 = x1.view(2, 3, 4)
+  x1_h = x1_h.view(2, 3, 4)
+
+  assert x1_h.device == torch.device("hammerblade")
+  assert torch.equal(x1_h.cpu(), x1)
+
+def test_view_4():
+  x1 = torch.rand(2,3)
+  x1_h = x1.hammerblade()
+
+  x1 = x1.view(3,2)
+  x1_h = x1_h.view(3,2)
+
+  assert x1_h.device == torch.device("hammerblade")
+  assert torch.equal(x1_h.cpu(), x1)
 
